13ea8f6a52f17e8e433f63780047ed5bd2135ddf
Hardik Patel <pm385@cummins.com>
2019-03-03 15:01:52 -0500

    creating model and implement reciepie list Item

--- shopping-list/src/app/recipes/recipe-list/recipe-list.component.ts
+++ shopping-list/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipes.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+	recipes: Recipe[] = [
+		new Recipe('A Test Recipe', 'This is simply a test', 'https://media.defense.gov/2015/Nov/16/2001318253/-1/-1/0/151123-A-YG824-001.JPG'),
+		new Recipe('A Test Recipe', 'This is simply a test', 'https://media.defense.gov/2015/Nov/16/2001318253/-1/-1/0/151123-A-YG824-001.JPG')
+	];
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
